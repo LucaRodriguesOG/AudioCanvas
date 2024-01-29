aacb2db9b8d2ffd4aa822a0ba8f2985df2eea500
Ryan Hernandez <86322787+CoolGuyRy@users.noreply.github.com>
2024-01-29 01:57:48 -0800

    Changes: - Implemented a "Hello World" for miniaudio - Added a couple songs for testing purposes. - Created a new branch for audio implementation

--- AudioCanvas/main.cpp
+++ AudioCanvas/main.cpp
@@ -1,16 +1,34 @@
-#include <iostream>
+#define MINIAUDIO_IMPLEMENTATION
+#include <miniaudio.h>
+
+#include <stdio.h>
 
 #include "Renderer.h"
-#include "Shader.h"
 
 int main() {
+	ma_result result;
+	ma_engine engine;
+
+	result = ma_engine_init(NULL, &engine);
+	if (result != MA_SUCCESS) {
+		return -1;
+	}
+
+	ma_engine_play_sound(&engine, "song-show-me-how.wav", NULL);
+
+	ma_engine_set_volume(&engine, 0.1f);
+
 	{
 		Renderer gRenderer;
 
 		gRenderer.Start();
 	}
+	
 
-	std::cin.get();
+	printf("Press Enter to quit...");
+	getchar();
+
+	ma_engine_uninit(&engine);
 
 	return 0;
 }
